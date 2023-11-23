0d56b874e206d0992256c357b32904b46fd51a63
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 13:50:55 +0100

    fix path

--- emulation/launch.sh
+++ emulation/launch.sh
@@ -6,8 +6,8 @@ qemu-system-aarch64 \
     -cpu cortex-a53 \
     -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
     -dtb bcm2710-rpi-3-b-plus.dtb \
-    -drive file=~/os/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
-    -kernel ~/os/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
+    -drive file=~/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
+    -kernel ~/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
     -m 1G -smp 4 \
     -serial stdio \
     -usb -device usb-mouse -device usb-kbd \
