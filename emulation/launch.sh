0f7cc334e16aaaa2ddea5e4fd0a1e50e72f69c5d
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 14:12:12 +0100

    delete vitio

--- emulation/launch.sh
+++ emulation/launch.sh
@@ -6,7 +6,7 @@ qemu-system-aarch64 \
     -cpu cortex-a53 \
     -append "$(cat /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bootfiles/cmdline.txt)" \
     -dtb  /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bcm2710-rpi-3-b-plus.dtb \
-    -drive format=raw,if=virtio,file=/home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
+    -drive format=raw,file=/home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
     -kernel /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
     -m 1G -smp 4 \
     -nographic
