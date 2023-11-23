8f0d8a964733ecdf67e8186513ee78bb210af3ce
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 14:08:02 +0100

    cleanup

--- emulation/launch.sh
+++ emulation/launch.sh
@@ -10,7 +10,4 @@ qemu-system-aarch64 \
     -kernel /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
     -m 1G -smp 4 \
     -serial stdio \
-    -usb -device usb-mouse -device usb-kbd \
     -nographic \
-    -device usb-net,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::5555-:22
