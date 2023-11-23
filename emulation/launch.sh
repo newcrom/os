1ae9cf71b4002ac7fb141f9840ac6c165092a145
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 14:07:15 +0100

    fix path

--- emulation/launch.sh
+++ emulation/launch.sh
@@ -4,10 +4,10 @@
 qemu-system-aarch64 \
     -M raspi3b \
     -cpu cortex-a53 \
-    -append "$(cat ~/os/build/tmp/deploy/images/raspberrypi3-64/bootfiles/cmdline.txt)" \
-    -dtb  ~/os/build/tmp/deploy/images/raspberrypi3-64/bcm2710-rpi-3-b-plus.dtb \
-    -drive format=raw,if=virtio,file=~/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
-    -kernel ~/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
+    -append "$(cat /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bootfiles/cmdline.txt)" \
+    -dtb  /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bcm2710-rpi-3-b-plus.dtb \
+    -drive format=raw,if=virtio,file=/home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
+    -kernel /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
     -m 1G -smp 4 \
     -serial stdio \
     -usb -device usb-mouse -device usb-kbd \
