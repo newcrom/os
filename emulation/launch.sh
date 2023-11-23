d611fb0f021bdad0b92ec289fef63218e025defe
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 14:33:46 +0100

    qemu script

--- emulation/launch.sh
+++ emulation/launch.sh
@@ -9,6 +9,6 @@ qemu-system-aarch64 \
     -append "$(cat /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bootfiles/cmdline.txt)" \
     -dtb  /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/bcm2710-rpi-3-b-plus.dtb \
     -drive format=raw,file=/home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
-    -kernel /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/Image-raspberrypi3-64.bin \
+    -kernel kernel8.img \
     -m 1G -smp 4 \
     -nographic
