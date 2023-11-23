a30830cf4ee108741ba50c6c3ec85bf0a688956d
Igor Fominykh <ifdotpy@gmail.com>
2023-11-23 14:24:45 +0100

    second qemu script change image

--- emulation/launch2.sh
+++ emulation/launch2.sh
@@ -7,7 +7,7 @@ qemu-system-aarch64 \
     -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
     -dtb bcm2710-rpi-3-b-plus.dtb \
     -sd disk.img \
-    -kernel kernel8.img \
+    -kernel /home/ssm-user/os/build/tmp/deploy/images/raspberrypi3-64/core-image-full-cmdline-raspberrypi3-64.rootfs-20231122164440.ext4 \
     -m 1G -smp 4 \
     -serial stdio \
     -usb -device usb-mouse -device usb-kbd \
